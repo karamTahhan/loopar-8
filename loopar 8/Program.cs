47cd72a470b7d2deb5e73f93d294aacbe0d2a1d4
karamTahhan <kata07001@utb.vaxjo.se>
2024-12-17 14:03:37 +0100

    Add project files.

---
+++ loopar 8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace loopar_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int produkt = 1;
+            for (int i = 1; i <=5; i++)
+            {
+                produkt *= i;
+                {
+                    Console.WriteLine("produkten av heltalen från 1 till 5 är;" + produkt);
+                }
+
+            }
+
+        }
+    }
+
+}
